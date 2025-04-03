969cfcf406ee2cd4142ded3259c788d478a6d483
FoxtrotDelta-1 <finn.docherty.2020@uni.strath.ac.uk>
2025-04-03 10:55:34 +0000

    doc strings for n_sphere

--- assignment_3/n_sphere.py
+++ assignment_3/n_sphere.py
@@ -5,11 +5,29 @@ Created on Tue Apr  1 13:23:12 2025
 @author: FinnghualaD
 """
 
+"""
+this file contains the function "n_sphere" this function computes the containted region/"area/volume" that an n dimensional ball takes up within a 
+n dimensional cube.
+"""
 import numpy as np
 
 def n_sphere(n_dimensions, n_samples):
     """
-    calculates the number get the gist down first
+    calculates the containted region/"area/volume" of an n-ball within an n-cube
+
+    inputs: n_dimensions is the number of dimensions for the n_ball
+            n_samples is the number of samples to be used in the calculation
+
+    an array n_samples by n_dimensions is created and populated with random numbers
+    the magnitude of each sample (row) is calculated by summing the squares of each 
+    entry in the row (n entries = n_dimensions).
+
+    if the sum of the squares is less than or equal to unity then the included array
+    enetry for that sample is flipped from a 0 to a 1.
+
+    included is an array of dimension n_samples.
+
+    returns: included
     """
 
     magnitude = 0
